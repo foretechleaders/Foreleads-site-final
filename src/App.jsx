33f6915e9b78e167a89f3c717a7bffed2e9d20d4
foretechleaders <foretechleaders@gmail.com>
2025-10-24 13:03:19 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
+
+// Import your pages
 import Home from './pages/Home'
 import About from './pages/About'
 import Services from './pages/Services'
@@ -10,34 +12,35 @@ import Resources from './pages/Resources'
 import Contact from './pages/Contact'
 import LeadershipArticles from './pages/LeadershipArticles'
 
-// Scroll to top on route change
-function ScrollToTop() {
-  const { pathname } = useLocation()
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [pathname])
-  return null
-}
-
 export default function App() {
   return (
     <Router>
-      <ScrollToTop />
-      <div className="flex flex-col min-h-screen">
+      <div className="flex flex-col min-h-screen bg-white">
+        {/* Fixed Header */}
         <Header />
 
-        <main className="flex-grow">
+        {/* Main Content */}
+        <main
+          className="
+            flex-grow 
+            mt-24 sm:mt-28 md:mt-32 
+            mb-16 sm:mb-20 md:mb-24 
+            px-4 sm:px-6 md:px-8 
+            max-w-7xl mx-auto w-full
+          "
+        >
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/services" element={<Services />} />
             <Route path="/programs" element={<Programs />} />
             <Route path="/resources" element={<Resources />} />
-            <Route path="/resources/articles" element={<LeadershipArticles />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/leadership-articles" element={<LeadershipArticles />} />
           </Routes>
         </main>
 
+        {/* Footer */}
         <Footer />
       </div>
     </Router>
